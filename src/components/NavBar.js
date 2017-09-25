cf8aaa6771b974a869042de230f27bb057b8734a
Gemma <gemmawvoakes@gmail.com>
2017-09-25 18:53:56 +0100

    Added simple navbar.

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../css/NavBar.css';
+
+const NavBar = function () {
+    return (
+        <nav className='navbar'>
+            <div className='container'>
+                <div className='navbar-brand'>
+                    <a className='navbar-item header-text' href="/">
+                        <img className="logo"
+                            src="https://cdn-images-1.medium.com/max/184/1*LdnSztHVYhhd8K8EqlgCJQ@2x.png" alt="Northcoders News logo" width="75" height="20" />
+                        Northcoders News
+                    </a>
+                </div>
+                <div className="navbar-tabs">
+                    <a className="navbar-item is-tab is-active" href="/">
+                        Home
+                    </a>
+                    <a className="navbar-item is-tab" href="#">
+                        All topics
+                        </a>
+                    <a className="navbar-item is-tab" href="#">
+                        Football
+                            </a>
+                    <a className="navbar-item is-tab" href="#">
+                        Cooking
+                            </a>
+                    <a className="navbar-item is-tab" href="#">
+                        Coding
+                            </a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
