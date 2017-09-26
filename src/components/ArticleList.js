552428cb411942da5d981e7d62c1cb488a701464
Gemma <gemmawvoakes@gmail.com>
2017-09-26 17:18:48 +0100

    Added axios to fetch article data from api.

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import axios from 'axios';
+
 import ArticleCard from './ArticleCard';
 
-const articles = [{ title: 'I\'m an article', votes: 4 },
-{ title: 'I\'m another article', votes: 5 },
-{ title: 'I\'m an article too', votes: 2 }];
+const path = 'http://localhost:3001/api/articles';
+
+// const articles = [{ title: 'I\'m an article', votes: 4 },
+// { title: 'I\'m another article', votes: 5 },
+// { title: 'I\'m an article too', votes: 2 }];
 
 class ArticleList extends React.Component {
-  render() {
+  constructor (props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false
+    };
+  }
+  componentDidMount () {
+    axios.get(path)
+    .then(articles => {
+      this.setState(
+        {articles: articles.data.articles}
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+  
+  render () {
+    const { articles} = this.state;
     return (
       <div id='ArticleList'>
         <h3 className='title is-3'>All Articles</h3>
