0e4f843443e00cfb7855238274d9e00f8747d0b7
Gemma <gemmawvoakes@gmail.com>
2017-10-03 12:56:47 +0100

    Tidied up NavBar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,32 +4,30 @@ import '../css/NavBar.css';
 const NavBar = function () {
   return (
     <nav className='navbar'>
-      <div className='container'>
-        <div className='navbar-brand'>
-          <a className='navbar-item header-text' href="/">
-            <img className="logo"
-              src="https://cdn-images-1.medium.com/max/184/1*LdnSztHVYhhd8K8EqlgCJQ@2x.png" alt="Northcoders News logo" width="75" height="20" />
-            Northcoders News
-          </a>
-        </div>
-        <div className="navbar-tabs">
-          <a className="navbar-item is-tab is-active" href="/">
-            Home
-          </a>
-          <a className="navbar-item is-tab" href="/topics">
-            All topics
-          </a>
-          <a className="navbar-item is-tab" href="/topics/football/articles">
-            Football
-          </a>
-          <a className="navbar-item is-tab" href="/topics/cooking/articles">
-            Cooking
-          </a>
-          <a className="navbar-item is-tab" href="/topics/coding/articles">
-            Coding
-          </a>
-        </div>
+
+      <div className='navbar-brand'>
+        <a className='navbar-item header-text is-centered' href="/">
+          <img className="logo"
+            src="https://cdn-images-1.medium.com/max/184/1*LdnSztHVYhhd8K8EqlgCJQ@2x.png" alt="Northcoders News logo" width="75" height="20" />
+          Northcoders News
+        </a>
       </div>
+      
+      <a className="navbar-item is-tab is-active" href="/">
+        Home
+      </a>
+      <a className="navbar-item is-tab" href="/topics">
+        All topics
+      </a>
+      <a className="navbar-item is-tab" href="/topics/football/articles">
+        Football
+      </a>
+      <a className="navbar-item is-tab" href="/topics/cooking/articles">
+        Cooking
+      </a>
+      <a className="navbar-item is-tab" href="/topics/coding/articles">
+        Coding
+      </a>
     </nav>
   );
 };
