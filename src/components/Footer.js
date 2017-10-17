ae7cfedc015fb1d08ff9ff4f4e7dc8a2a0376054
Gemma <gemmawvoakes@gmail.com>
2017-10-17 17:41:02 +0100

    [Footer] component added.

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = function () {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="content has-text-centered">
+          <p>
+            <strong>Northcoders News</strong> by <a href="https://github.com/Gemmawv" target="_blank">Gemma Voakes</a>.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
