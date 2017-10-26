adce80babbfa4c555f3095271917346360fb1a00
Gemma <gemmawvoakes@gmail.com>
2017-10-26 16:53:25 +0100

    Add build to .gitignore

--- src/config.js
+++ src/config.js
@@ -3,6 +3,10 @@ module.exports = {
     ROOT: 'http://localhost:4000/api',
     USERNAME: 'northcoder'
   },
+  test: {
+    ROOT: 'http://localhost:4000/api',
+    USERNAME: 'northcoder'
+  },
   production:{
     ROOT: 'https://nameless-sands-36123.herokuapp.com/api',
     USERNAME: 'northcoder'
